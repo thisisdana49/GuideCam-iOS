f7b79c84897e46ac0f67dfe1e3fbb0bc54349bfd
thisisdana49 <thisisdana49@gmail.com>
2025-04-03 13:57:15 +0900

    chore: 새로운 프로젝트 생성 및 gitignore 추가 #1

---
+++ GuideCam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuideCam
+//
+//  Created by 조다은 on 4/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
