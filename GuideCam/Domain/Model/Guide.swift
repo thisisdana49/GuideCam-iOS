cb4e5a70fab79d1bd6f292af4d92d3b128a5cbae
thisisdana49 <thisisdana49@gmail.com>
2025-04-04 21:01:26 +0900

    design: 가이드 리스트 화면 View 및 ViewController 구성 #7

---
+++ GuideCam/Domain/Model/Guide.swift
@@ -0,0 +1,15 @@
+//
+//  Guide.swift
+//  GuideCam
+//
+//  Created by 조다은 on 4/4/25.
+//
+
+import Foundation
+
+struct Guide {
+    let title: String
+    let thumbnailPath: String
+    let isFavorite: Bool
+    let isRecent: Bool
+}
